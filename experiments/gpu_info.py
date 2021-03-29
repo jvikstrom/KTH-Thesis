cec23f8f874fb57975863dd9df8c7518f3f21d99
Johan Vikström <johan.vikstrom7@gmail.com>
2021-03-29 13:18:41 +0200

    Removed experiments subdirectory.

--- experiments/gpu_info.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-print(f"Tensorflow version:  {tf.__version__}")
-physical_devices = tf.config.list_physical_devices('GPU')
-print("Num GPUs:", len(physical_devices))
-print("GPU info: ", physical_devices)
